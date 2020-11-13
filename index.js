2e0af8c61374bf62d9bfc96376a149a66779a59f
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-11-13 18:16:35 -0500

    Completed header

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+// hamburger functionality
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+hamburger.addEventListener('click', () => {
+  nav.classList.contains('hidden')
+    ? nav.classList.remove('hidden')
+    : nav.classList.add('hidden');
+});
