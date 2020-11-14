9840f265ea390a72634920fbdf44b18ec811115e
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-11-14 06:51:58 -0500

    header shrinks on scroll

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ hamburger.addEventListener('click', () => {
     ? nav.classList.remove('hidden')
     : nav.classList.add('hidden');
 });
+
+// header shrink on scroll
+const header = document.querySelector('header');
+window.onscroll = () => {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    header.classList.add('header-shrink');
+  } else {
+    header.classList.remove('header-shrink');
+  }
+};
