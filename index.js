8d546c887f2448b36aa75d8ec8d523ea031e2c71
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-11-15 05:09:45 -0500

    added logic to check scroll height on page load

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ hamburger.addEventListener('click', () => {
     : nav.classList.add('hidden');
 });
 
-// header shrink on scroll
+// header shrink if not scrolled to the top
 const header = document.querySelector('header');
-window.onscroll = () => {
+const checkScroll = () => {
   if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
     header.classList.add('header-shrink');
   } else {
     header.classList.remove('header-shrink');
   }
+};
+checkScroll(); // check on page load
+window.onscroll = () => {
+  checkScroll(); // check on scroll
 };
