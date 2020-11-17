84a41cba567f20c536596b18842d8fb7251c2443
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-11-17 07:27:18 -0500

    set margin-top of main to header height

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ hamburger.addEventListener('click', () => {
 // header shrink if not scrolled to the top
 const header = document.querySelector('header');
 const checkScroll = () => {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+  if (
+    document.body.scrollTop > header.clientHeight ||
+    document.documentElement.scrollTop > header.clientHeight
+  ) {
     header.classList.add('header-shrink');
   } else {
     header.classList.remove('header-shrink');
@@ -20,3 +23,7 @@ checkScroll(); // check on page load
 window.onscroll = () => {
   checkScroll(); // check on scroll
 };
+
+// set margin top of main to the header height
+const main = document.querySelector('main');
+main.style.marginTop = header.clientHeight + 'px';
